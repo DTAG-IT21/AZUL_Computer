199439a180bbf6ff95cc05ec8d891c47f0a2aa05
Richone01 <leonrichter01@gmail.com>
2023-11-09 13:54:54 +0100

    Implemented class Manufacture

---
+++ src/de/dhbw/dtag_it21/azul/Manufacture.java
@@ -0,0 +1,25 @@
+package de.dhbw.dtag_it21.azul;
+
+public class Manufacture {
+    static final int MAX_TILES = 4;
+    private Tile[] tiles = new Tile[MAX_TILES];
+
+    public Manufacture() {
+    }
+
+    public Tile[] getTiles() {
+        return tiles;
+    }
+
+    public void setTiles(Tile[] tiles) throws RuntimeException {
+        if(tiles.length != 4){
+            throw new RuntimeException("Too many or too few tiles");
+        }
+        this.tiles = tiles;
+    }
+
+    public void empty(){
+        //Better than null?
+        tiles = new Tile[MAX_TILES];
+    }
+}
