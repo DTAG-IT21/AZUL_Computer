89a5c6dad75bf6b37bcdebadd8f78e2484235492
Richone01 <leonrichter01@gmail.com>
2024-04-16 14:58:01 +0200

    ✅ Updated ScoreCalculator horizontal test
    Changed position of placed tile so left and right check is included

--- src/test/java/test/de/studi/azulcomputer/ScoreCalculatorTest.java
+++ src/test/java/test/de/studi/azulcomputer/ScoreCalculatorTest.java
@@ -17,10 +17,10 @@ public class ScoreCalculatorTest {
     public void horizEval_multiple_Tiles() {
         int actual_points = 4;
         Tile[][] mosaic = new Tile[5][5];
+        mosaic[0][0] = new Tile(0);
         mosaic[0][1] = new Tile(1);
-        mosaic[0][2] = new Tile(2);
         mosaic[0][3] = new Tile(3);
-        int test_points = ScoreCalculator.moveEval(mosaic, 0, new Tile(0));
+        int test_points = ScoreCalculator.moveEval(mosaic, 0, new Tile(2));
         assert (actual_points == test_points);
     }
 }
