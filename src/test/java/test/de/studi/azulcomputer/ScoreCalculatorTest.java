0aa2f9637e96c600920880d7d1348e353e02cba4
Richone01 <leonrichter01@gmail.com>
2024-04-16 15:17:47 +0200

    ✅ Added ScoreCalculator horizontal tests
    Added test with spaces and with bonus points

--- src/test/java/test/de/studi/azulcomputer/ScoreCalculatorTest.java
+++ src/test/java/test/de/studi/azulcomputer/ScoreCalculatorTest.java
@@ -4,13 +4,15 @@ import de.studi.azulcomputer.backend.ScoreCalculator;
 import de.studi.azulcomputer.backend.Tile;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ScoreCalculatorTest {
     @Test
     public void horizEval_single_Tile() {
         int actual_points = 1;
         Tile[][] mosaic = new Tile[5][5];
         int test_points = ScoreCalculator.moveEval(mosaic, 0, new Tile(0));
-        assert (actual_points == test_points);
+        assertEquals (actual_points, test_points);
     }
 
     @Test
@@ -21,6 +23,30 @@ public class ScoreCalculatorTest {
         mosaic[0][1] = new Tile(1);
         mosaic[0][3] = new Tile(3);
         int test_points = ScoreCalculator.moveEval(mosaic, 0, new Tile(2));
-        assert (actual_points == test_points);
+        assertEquals(actual_points, test_points);
+    }
+
+    @Test
+    public void horizEval_multiple_Tiles_with_spaces() {
+        int actual_points = 3;
+        Tile[][] mosaic = new Tile[5][5];
+        mosaic[0][0] = new Tile(0);
+        mosaic[0][1] = new Tile(1);
+        mosaic[0][4] = new Tile(4);
+        int test_points = ScoreCalculator.moveEval(mosaic, 0, new Tile(2));
+        assertEquals (actual_points, test_points);
+    }
+
+    @Test
+    public void horizEval_bonus_points(){
+        int actual_points = 10;
+        Tile[][] mosaic = new Tile[5][5];
+        mosaic[0][0] = new Tile(0);
+        mosaic[0][1] = new Tile(1);
+        mosaic[0][2] = new Tile(2);
+        mosaic[0][4] = new Tile(4);
+        int test_points = ScoreCalculator.moveEval(mosaic, 0, new Tile(3));
+        assertEquals (actual_points, test_points);
+
     }
 }
