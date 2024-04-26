3a714256f8d7ab93add9537f19a4cd1a2a5c34a5
Richone01 <leonrichter01@gmail.com>
2024-04-26 09:16:30 +0200

    💄 The UI now scales with the client's screen size

--- _0_AZUL_Computer_plugin_database/src/main/java/de/studi/azulcomputer/plugin/database/MainApp.java
+++ _0_AZUL_Computer_plugin_database/src/main/java/de/studi/azulcomputer/plugin/database/MainApp.java
@@ -2,8 +2,11 @@ package de.studi.azulcomputer.plugin.database;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -13,9 +16,11 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/de/studi/azulcomputer/frontend/main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1600, 1024);
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        Scene scene = new Scene(fxmlLoader.load(), screenBounds.getMaxX(), screenBounds.getMaxY());
         stage.setTitle("Azul Computer");
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 }
